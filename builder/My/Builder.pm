05f6294debe6642738436f57ee3c60aaae1ce3d8
Joel Berger <joel.a.berger@gmail.com>
2011-10-18 23:11:38 +0000

    Import of JBERGER/Alien-GSL-0.0301 from CPAN.
    gitpan-cpan-distribution: Alien-GSL
    gitpan-cpan-version:      0.0301
    gitpan-cpan-path:         JBERGER/Alien-GSL-0.0301.tar.gz
    gitpan-cpan-author:       JBERGER
    gitpan-cpan-maturity:     released

---
+++ builder/My/Builder.pm
@@ -0,0 +1,311 @@
+package My::Builder;
+
+use strict;
+use warnings;
+
+use lib 'builder';
+
+use parent 'Module::Build';
+
+use Carp;
+
+use Scalar::Util 'blessed';
+use File::Temp ();
+use File::chdir;
+use HTTP::Tiny;
+use Net::FTP;
+use Archive::Extract;
+use Capture::Tiny 'capture';
+
+our $FTP_SERVER = 'ftp.gnu.org';
+our $FTP_FOLDER = '/gnu/gsl';
+our $CMD_GSL_CONFIG = 'gsl-config';
+
+## Generic Methods ##
+
+sub new {
+  my $class = shift;
+
+  my $self = $class->SUPER::new(@_);
+
+  bless($self, $class);
+
+  # default to a share_dir install
+  # My::Builder::Unix overrides this to make system default
+
+  $self->config_data(location => 'share_dir');
+
+  return $self;
+}
+
+sub have_gsl_version {
+
+  no warnings 'exec';
+  
+  my $gsl_version;
+  
+  capture {
+    $gsl_version = qx/ $CMD_GSL_CONFIG --version /;
+  };
+  
+  if ($?) {
+    $gsl_version = 0;
+    warn "Cannot execute '$CMD_GSL_CONFIG --version' or you do not have GSL installed\n";
+  }
+
+  chomp $gsl_version;
+
+  return $gsl_version;
+
+}
+
+sub ACTION_code {
+  my $self = shift;
+
+  my $have_version = $self->have_gsl_version;
+
+  if ( $have_version and ! $self->args('Force') and ! $self->args('ShareDir') ) {
+    $self->config_data( location => 'system' );
+  } else  {
+    my $download_dir = $self->get_download_dir();
+
+    my $dir = $self->fetch($download_dir, $self->args('Version'));
+
+    if ( $self->gsl_make_install($dir) ) {
+      print "Build/Install libgsl succeeded\n"; 
+      if ($self->config_data('location') eq 'share_dir') {
+        $self->set_share_dir_data();
+      }
+    } else {
+      print "Build/Install libgsl failed\n";
+    }
+  }
+  
+  unless ($self->config_data('location')) {
+    $self->config_data(location => 'unknown');
+    warn "Error unknown location, please fix!";
+  }
+
+  $self->SUPER::ACTION_code;
+}
+
+sub get_download_dir {
+  my $self = shift;
+
+  if ($self->args('Dir')) {
+    return $self->args('Dir');
+  }
+
+  my $temp_dir = $self->args('TempDir');
+  if ($temp_dir) {
+    return File::Temp->newdir(DIR => $temp_dir);
+  } else {
+    return File::Temp->newdir();
+  }
+}
+
+#sub gsl_make_install {
+#  my $self = shift;
+#  carp "Build/Install of GSL not available on this system";
+#  return 0;
+#}
+
+sub order_available {
+  my $self = shift;
+  my ($available) = @_;
+  croak "must supply one argument to order_available" unless $available;
+
+  my @order = 
+    map { $_->[0] }
+    sort { 
+      push @$a, 0 while @$a < 4;
+      push @$b, 0 while @$b < 4;
+      $a->[1] <=> $b->[1] ||
+      $a->[2] <=> $b->[2] ||
+      $a->[3] <=> $b->[3]
+    }
+    map {  
+      [ $_, split /\./ ]
+    }
+    keys %$available;
+
+  return @order;
+}
+
+sub available {
+  # available points to available_source unless overridden
+  my $self = shift;
+  return $self->available_source(@_);
+}
+
+sub fetch {
+  my $self = shift;
+  my ($dir, $version) = @_;
+
+  my $available = $self->available();
+  my @order_available = $self->order_available($available);
+
+  if ($version) {
+    unless (exists $available->{$version}) {
+      croak "Could not find a GSL version: $version, available are @order_available";
+    }
+  } else {
+    $version = $order_available[-1];
+    print "Found newest version: $version\n";
+  } 
+
+  my $from = $available->{$version}{from};
+  my $file = $available->{$version}{file};
+  $self->config_data( version => $version);
+
+  local $CWD = "$dir";
+
+  print "Attempting to download: $file\n";
+  if (blessed $from and $from->isa('Net::FTP') ) {
+    $from->binary();
+    $from->get( $file ) or croak "Download failed: " . $from->message();
+  } else {
+    my $response = HTTP::Tiny->new->mirror( $from . $file, $file );
+    croak "Download failed: " . $response->{reason} unless $response->{success};
+  }
+
+  print "Extracting $file\n";
+  my $ae = Archive::Extract->new( archive => $file );
+  $ae->extract;
+
+  print "Removing archive\n";
+  $ae = undef;
+  unlink($file) or carp "Could not remove archive $file";
+
+  my $extract_dir = $CWD;
+  if ($file =~ /(gsl-[\d\.]+)\.tar\.gz/) {
+    if (-d $1) {
+      local $CWD = $1;
+      $extract_dir = $CWD;
+    }
+  }
+
+  return $extract_dir;
+}
+
+sub local_exec_prefix {
+  return './';
+}
+
+## Source Methods ##
+
+sub available_source {
+  my $self = shift;
+
+  my $ftp = Net::FTP->new($FTP_SERVER, Debug => 0)
+    or croak "Cannot connect to some.host.name: $@";
+
+  $ftp->login() or croak "Cannot login ", $ftp->message;
+
+  $ftp->cwd($FTP_FOLDER) or croak "Cannot change working directory ", $ftp->message;
+
+  my @tarballs = grep {/^gsl-[\d\.]+\.tar\.gz$/} $ftp->ls();
+  croak "Could not find any tarballs at $FTP_SERVER$FTP_FOLDER" unless @tarballs;
+
+  my %available = 
+    map { 
+      my $version = $1 if /gsl-([\d\.]+)\.tar\.gz/; 
+      ($version => {from => $ftp, file => $_ }) 
+    }
+    @tarballs;
+
+  return \%available;
+
+}
+
+sub gsl_make_install {
+  my $self = shift;
+  my ($dir) = @_;
+
+  local $CWD = $dir if $dir;
+  print "Configuring GSL\n";
+  # check that this folder contains a configure script
+  croak "Folder does not contain AutoConf scripts" unless (-e 'configure');
+
+  my $configure_command = $self->local_exec_prefix() . 'configure';
+  my $is_root = ($< != 0);
+
+  if ($self->args('ShareDir') or ! $is_root) {
+    print "Using install method: ShareDir\n";
+
+    # for share_dir install get full path to share_dir
+    local $CWD = $self->base_dir();
+    push @CWD, 'share_dir';
+    $configure_command .= " --prefix=$CWD";
+
+    $self->config_data( location => 'share_dir' );
+
+  } else {
+
+    $self->config_data( location => 'system' );
+
+  }
+
+  system( $configure_command );
+  if ($?) {
+    print "Configure ($configure_command) Failed!\n";
+    return 0;
+  }
+
+  print "Building GSL\n";
+  system( 'make' );
+  if ($?) {
+    print "Build (make) Failed!\n";
+    return 0;
+  }
+
+  print "Installing GSL\n";
+  system( 'make install' );
+  if ($?) {
+    print "Install (make install) Failed!\n";
+    return 0;
+  }
+
+  return 1;
+
+}
+
+## Pre-compiled Methods ##
+
+sub available_compiled {
+  my $self = shift;
+  croak "Pre-compiled GSL libraries are not available for this system";
+}
+
+## System Install Methods ##
+
+## ShareDir Methods ##
+
+sub set_share_dir_data {
+  my $self = shift;
+
+  #local $CWD;
+  #push @CWD, qw'share_dir bin';
+  #my $base_command = $self->local_exec_prefix() . 'gsl-config';
+
+  #{
+    # emulate gsl-config --libs
+
+    #my $command = $base_command . ' --libs';
+    #my $libs_str = qx/$command/;
+    #if ($?) {
+    #  carp "Could not execute $command: $!";
+    #  $libs_str = '';
+    #}
+
+    #chomp($libs_str);
+    #my @libs = grep { ! /^-L/ } split(/ /, $libs_str);
+	my @libs = qw( -lgsl -lgslcblas -lm ); # hard code libs rather than determine
+    $self->config_data( libs => \@libs );
+
+  #}
+  
+}
+
+1;
+
--- lib/Alien/GSL.pm
+++ lib/Alien/GSL.pm
@@ -3,14 +3,17 @@ package Alien::GSL;
 use strict;
 use warnings;
 
-our $VERSION = 0.02;
+our $VERSION = 0.03_01;
 $VERSION = eval $VERSION;
 
 use Carp;
-use LWP::Simple;
-use File::Temp ();
+use Alien::GSL::ConfigData;
+use File::ShareDir 'module_dir';
 use File::chdir;
-use Archive::Extract;
+
+# if $location eq share_dir then the module was built in share_dir mode 
+
+my $share_dir = (Alien::GSL::ConfigData->config('location') eq 'share_dir') ? module_dir('Alien::GSL') : '';
 
 =head1 NAME 
 
@@ -28,111 +31,84 @@ This module is meant to ease the install of the Gnu Scientific Library (GSL). It
    die "This module requires at least GSL 1.15";
  }
 
- -- or perhaps --
+=head1 INSTALLATION
 
- unless (Alien::GSL::have_gsl_version()) {
-   Alien::GSL::install() or die "Cannot install GSL";
- }
+L<Alien::GSL> uses the L<Module::Build> system for installation. Therefore the usual build process is
 
-=head1 EXPORTS
+ perl Build.PL
+ ./Build
+ ./Build test
+ ./Build install
 
-Currently this module does not export any functions or variables. Use instead the fully qualified symbol name, i.e. C<Alien::GSL::install()> or C<@Alien::GSL::SUPPORTED_OSES>.
+It will try (at a minimum) to detect if the GSL library is installed on the local system. If not it will attempt, if possible, to download/build/install it. This build process will likely require the C<Build> script to be run with root privaledges. Future versions of L<Alien::GSL> may try to avoid this problem. This is not necessary if the library is already installed on the system.
 
-=head1 INTERFACE STABILITY
+=head2 Build Flags
 
-This module is in an alpha state. The author hopes that major functionality will remain. Of particular note is the testability of the installation process. Further at this point only Linux platforms can download, build and install the GSL library. All other platforms will die during configure (C<perl Makefile.PL>) stage if GSL cannot be found. The author hopes to expand any other possible functionality.
-
-=head1 PACKAGE VARIABLES
+When running C<./Build>, certain command line flags may be passed, i.e. C<./Build --ShareDir>.
 
 =over
 
-=item * 
+=item C<--Version 1.15>
 
-C<$FTP_ROOT> - specifies the FTP site where the GSL library is available. Note: This variable should end in a trailing slash.
+Specify a version of the GSL library to be installed (here C<1.15>). Without this flag, the highest (read: newest) version available will be used. If a supplied version cannot be found, the install will croak.
 
-=item *
+=item C<--ShareDir>
 
-C<$CMD_GSL_CONFIG_VERSION> - the command that is run to check the version of the installed GSL library. Note that (for now) this variable is only checked in the C<have_gsl_version> and C<required_gsl_version> functions, NOT the C<version> function.
+When this flag is given, C<File::ShareDir> will be used, even if a system install was possible (i.e. on Linux as root).
 
-=item *
+=item C<--Force>
 
-C<@SUPPORTED_OSES> - lists the OSes on which the C<Alien::GSL> module can install the GSL library. On these OSes the C<install> function will attempt to install the library, using an OS specific subservient function.
+When this flad is given, action will be taken, even if a system install of GSL is found. Note that this flag is not needed if C<--ShareDir> is used.
+
+=item C<--Dir dir>
+
+Specify a directory (here C<dir>) to download and build the library. This directory will not be removed later.
+
+=item C<--TempDir /dev/shm>
+
+Specify a location for the temporary (here C</dev/shm/>, the ramdisk on Ubuntu Linux).
 
 =back
 
-=cut
+=head1 NO EXPORTS
 
-our $FTP_ROOT = 'ftp://ftp.gnu.org/gnu/gsl/';
-our $CMD_GSL_CONFIG_VERSION = 'gsl-config --version';
+Currently this module does not export any functions or variables. Use instead the fully qualified symbol name, i.e. C<Alien::GSL::gsl_version()>.
 
-#once you create a branch in in Alien::GSL::install for a new OS
-#enter that OS indicator (return from $^O) here
-our @SUPPORTED_OSES = ( qw/ 
-  linux
-/ );
+=head1 INTERFACE STABILITY
 
-=head1 INSTALLING FUNCTIONS
+This module is in an alpha state. The author hopes that major functionality will remain. The module now uses L<Module::Build> which allows the install functionality (download, build, install) to be platform specific and separated from the usage functionality described in the L<MODULE FUNCTIONS> section.
 
-=head2 available
+=head1 MODULE FUNCTIONS
 
-Takes no parameters. In list context returns an array of the GSL tarballs available from the FTP folder given in the C<$FTP_ROOT> variable. In scalar context returns only the tarball with the highest version number. 
+These functions are basically a functional interface to the C<gsl-config> utility command.
 
-By default the C<$FTP_ROOT> and this tarball name may be joined to form a full download location. If the user specifies a different C<$FTP_ROOT>, be sure to include a trailing slash.
+=head2 C<gsl_version>
+
+Takes no options, returns the version number of the installed GSL library.
 
 =cut
 
-sub available {
-  my $index = get( $FTP_ROOT );
+sub gsl_version {
+  my $version;
 
-  my @tarballs = ($index =~ /(gsl-[\d\.]+\.tar\.gz)(?!\.sig)/g);
-  @tarballs = 
-    map { $_->[0] }
-    sort { 
-      push @$a, 0 while @$a < 4;
-      push @$b, 0 while @$b < 4;
-      $a->[1] <=> $b->[1] ||
-      $a->[2] <=> $b->[2] ||
-      $a->[3] <=> $b->[3]
-    }
-    map { 
-      my $version = $1 if /-([\d\.]+)\./; 
-      [ $_ , split(/\./, $version) ]
-    }
-    @tarballs;
-
-  if (wantarray) {
-    return @tarballs;
+  if ($share_dir) {
+    $version = Alien::GSL::ConfigData->config('version');
   } else {
-    return $tarballs[-1];
+    $version = qx/ gsl-config --version /;
+    if ($?) {
+      carp "Call to gsl-config --version failed: $!";
+      $version = '';
+    }
+
+    chomp($version);
   }
 
+  return $version;
 }
 
-=head2 have_gsl_version
+=head2 C<require_gsl_version( [$version] )>
 
-Takes no parameters, returns the installed version of the GSL library or zero if C<gsl-config> cannot be executed on the system.
-
-=cut
-
-sub have_gsl_version {
-
-  no warnings 'exec';
-
-  my $gsl_version = qx/ $CMD_GSL_CONFIG_VERSION /;
-  if ($?) {
-    $gsl_version = 0;
-    carp "Cannot execute $CMD_GSL_CONFIG_VERSION or you do not have GSL installed";
-  }
-
-  chomp $gsl_version;
-
-  return $gsl_version;
-
-}
-
-=head2 require_gsl_version( [$version] );
-
-A wrapper around C<have_gsl_version()> which (optionally) takes a number specifying a minimum GSL version, returns the GSL version if it is greater than or equal to that specified. Returns zero otherwise. May also be called with zero as the version parameter, or no parameter at all, in which case the behavior is the same as C<have_gsl_version()>.
+A wrapper around C<gsl_version()> which (optionally) takes a number specifying a minimum GSL version, returns the GSL version if it is greater than or equal to that specified. Returns zero otherwise. May also be called with zero as the version parameter, or no parameter at all, in which case the behavior is the same as C<gsl_version()>.
 
 =cut
 
@@ -141,7 +117,7 @@ sub require_gsl_version {
   my $required = shift;
   $required ||= 0;
 
-  my $have = Alien::GSL::have_gsl_version();
+  my $have = gsl_version();
 
   if ($required == 0) {
     return $have if $have;
@@ -155,133 +131,31 @@ sub require_gsl_version {
 
 }
 
-=head2 install ( [$version,] [$opts_hashref] );
-
-Delegation function which calls the OS specific install function. Those functions may be called directly but as it is not recommended they are not listed in this document.
-
-May take up to two arguments, a parameter which specifies the desired version to install (i.e. C<1.15> or C<1.1.1>) which must correspond exactly to the version number in the filename of the tarball. The final argument may be hash reference with options. Currently the available options are:
-
-=over
-
-=item *
-
-C<CLEANUP> - if set to a true value the temporary folder create will be removed upon the completion of the script. This is the default.
-
-=back
-
-This function returns zero if the build/install fails and the version number (as returned by C<have_gsl_version()>) if the build/install succeeds.
-
-On *nix systems this function can only be run with root permissions. Other operating systems (if supported) may require elevated permissions as well.
-
-=cut
-
-sub install {
-  # this function can be used to delegate to individual 
-  # OS specific installers once they are created
-  # this should test $^O. Be sure to update @SUPPORTED_OSES too!
-
-  # perhaps setting CBLAS env variable should go here? is that OS indep?
-
-  if ($^O eq 'linux') {
-    goto &install_linux;
-  } else {
-    return 0;
-  }
-}
-
-sub install_linux {
-
-  # check if running as root
-  if ($< != 0) {
-    print "Must call Alien::GSL::install with root permissions\n";
-    return 0;
-  }
-
-  my $opts = (@_ and ref $_[-1] eq 'HASH') ? pop : {};
-
-  my $version = shift;
-  my $file;
-  if (defined $version) {
-    my @available = grep { /gsl-$version\.tar\.gz/ } Alien::GSL::available();
-    if (@available > 1) {
-      croak "Could not uniquely determine desired version. Files which match your specification (from GSL FTP) are: @available\n";
-    } elsif (@available == 0) {
-      croak "Could not find desired version on the GSL FTP ftp server";
-    } else {
-      $file = $available[0];
-    }
-  } else {
-    $file = Alien::GSL::available();
-  }
-
-  my $dir = File::Temp->newdir( CLEANUP => defined $opts->{CLEANUP} ? $opts->{CLEANUP} : 1 );
-  print "Using temporary directory: $dir\n";
-
-  {
-
-    local $CWD = "$dir";
-
-    print "Attempting to download: $FTP_ROOT$file\n";
-    getstore( $FTP_ROOT . $file, $file );
-
-    print "Extracting $file\n";
-    my $ae = Archive::Extract->new( archive => $file );
-    $ae->extract;
-    (my $extract_dir = $file) =~ s/(gsl-[\d\.]+)\.tar\.gz/$1/;
-
-    {
-      local $CWD = $extract_dir;
-
-      print "Configuring GSL\n";
-      system( './configure' );
-      if ($?) {
-        print "Configure Failed!\n";
-        return 0;
-      }
-
-      print "Building GSL\n";
-      system( 'make' );
-      if ($?) {
-        print "Build (make) Failed!\n";
-        return 0;
-      }
-
-      print "Installing GSL\n";
-      system( 'make install' );
-      if ($?) {
-        print "Install (make install) Failed!\n";
-        return 0;
-      }
-    }
-
-    #back in base temporary directory $dir
-
-  }
-
-  return Alien::GSL::have_gsl_version();
-
-}
-
-=head1 MODULE FUNCTIONS
-
-These functions are basically a functional interface to the C<gsl-config> utility command.
-
-=head2 gsl_prefix
+=head2 C<gsl_prefix>
 
 Takes no options, returns the "GSL installation prefix".
 
 =cut
 
 sub gsl_prefix {
-  my $prefix = qx/ gsl-config --prefix /;
-  if ($?) {
-    warn "Call to gsl-config --prefix failed: $!";
+  my $prefix;
+
+  if ($share_dir) {
+    return $share_dir;
+  } else {
+    $prefix = qx/ gsl-config --prefix /;
+    if ($?) {
+      carp "Call to gsl-config --prefix failed: $!";
+      $prefix = '';
+    }
+
+    chomp($prefix);
   }
 
   return $prefix;
 }
 
-=head2 gsl_libs( [opts hash or hash reference] )
+=head2 C<gsl_libs( [opts hash or hash reference] )>
 
 Takes an optional hash or hash reference, returns "library linking information". A hash key C<cblas>, whose value is false will return the "library linking information, without cblas", though by default the cblas information is included.
 
@@ -289,53 +163,95 @@ Takes an optional hash or hash reference, returns "library linking information".
 
 sub gsl_libs {
   my %opts = (ref $_[0] eq 'HASH') ? %{ $_[0] } : @_;
-  my $call = 'gsl-config --libs';
 
   if (! defined $opts{cblas}) {
     $opts{cblas} = 1;
   }
-  
-  if ($opts{cblas} == 0) {
-    $call .= '-without-cblas';
-  } 
 
-  my $prefix = qx/ $call /;
-  if ($?) {
-    warn "Call to $call failed: $!";
+  my $libs;
+  if ($share_dir) {
+
+    my @libs = Alien::GSL::ConfigData->config('libs');
+
+    unless ($opts{cblas}) {
+      @libs = grep { ! /cblas/ } @libs;
+    }
+
+    {
+      local $CWD = $share_dir;
+      push @CWD, 'lib';
+      unshift @libs, '-L' . $CWD;
+    }
+
+    $libs = join(' ', @libs);
+  } else {
+
+    my $call = 'gsl-config --libs';
+
+    unless ($opts{cblas}) {
+      $call .= '-without-cblas';
+    } 
+
+    $libs = qx/ $call /;
+    if ($?) {
+      carp "Call to $call failed: $!";
+    }
+
+    chomp($libs);
   }
 
-  return $prefix;
+  return $libs;
 }
 
-=head2 gsl_cflags
+=head2 C<gsl_cflags>
 
 Takes no options, returns the "pre-processor and compiler flags".
 
 =cut
 
 sub gsl_cflags {
-  my $cflags = qx/ gsl-config --cflags /;
-  if ($?) {
-    warn "Call to gsl-config --cflags failed: $!";
+  my $cflags;
+
+  if ($share_dir) {
+    local $CWD = $share_dir;
+    push @CWD, 'include';
+    $cflags = '-I' . $CWD;
+
+  } else {
+
+    my $command = 'gsl-config --cflags';
+    $cflags = qx/$command/;
+    if ($?) {
+      carp "Call to $command failed: $!";
+    }
+
+    chomp($cflags);
   }
 
   return $cflags;
 }
 
-=head2 gsl_version
+=head1 TODO
 
-Takes no options, returns "version information". This function is provided for symmetry, however, for flexibility and error handling the C<have_gsl_version()> function is recommended, especially in pre-install usage.
+=over
 
-=cut
+=item *
 
-sub gsl_version {
-  my $version = qx/ gsl-config --version /;
-  if ($?) {
-    warn "Call to gsl-config --version failed: $!";
-  }
+Implement C<--GSLCheck> flag to run C<make check> during build.
 
-  return $version;
-}
+=item *
+
+Clean the C<share_dir> directory when C<ACTION_clean> is run.
+
+=item *
+
+Improve tests for C<Alien::GSL>.
+
+=item *
+
+Are tests possible for C<Module::Build> subclasses?
+
+=back
 
 =head1 SEE ALSO
 
